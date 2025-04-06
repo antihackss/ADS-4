92bab29348630ed807a2dafce73bccd6ab448680
antihackss <vladmakov0859@gmail.com>
2025-04-06 16:03:48 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,54 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = 0; i < len; ++i) {
+    for (int j = i + 1; j < len; ++j) {
+      if (arr[i] + arr[j] == value) {
+        ++count;
+      }
+    }
+  }
+  return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  int left = 0, right = len - 1;
+  while (left < right) {
+    int sum = arr[left] + arr[right];
+    if (sum == value) {
+      ++count;
+      ++left;
+      --right;
+    } else if (sum < value) {
+      ++left;
+    } else {
+      --right;
+    }
+  }
+  return count;
+}
+int binarySearch(int *arr, int len, int value, int startIndex) {
+  int left = startIndex + 1, right = len - 1;
+  while (left <= right) {
+    int mid = left + (right - left) / 2;
+    if (arr[mid] == value) {
+      return mid;
+    } else if (arr[mid] < value) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = 0; i < len - 1; ++i) {
+    int secondElement = value - arr[i];
+    int index = binarySearch(arr, len, secondElement, i);
+    if (index != -1 && index > i) {
+      ++count;
+    }
+  }
+  return count;
 }
