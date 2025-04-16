370dbc00fc585c289a5083a16c0cd712bf0b37e4
antihackss <vladmakov0859@gmail.com>
2025-04-16 14:32:25 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,4 +1,5 @@
 // Copyright 2025 NNTU-CS
+
 #include <algorithm>
 
 int countPairs1(int *arr, int len, int value) {
@@ -17,37 +18,48 @@ int countPairs2(int *arr, int len, int value) {
     int count = 0;
     int left = 0;
     int right = len - 1;
+    
     while (left < right) {
         int sum = arr[left] + arr[right];
+        
         if (sum == value) {
             if (arr[left] == arr[right]) {
-                int num = right - left + 1;
-                count += num * (num - 1) / 2;
+                int n = right - left + 1;
+                count += n * (n - 1) / 2;
                 break;
             }
+
             int left_val = arr[left];
-            int right_val = arr[right];
-            int left_count = 0, right_count = 0;
-            while (left < len && arr[left] == left_val) {
+            int left_count = 1;
+            while (left + 1 < len && arr[left + 1] == left_val) {
                 left++;
                 left_count++;
             }
-            while (right >= 0 && arr[right] == right_val) {
+
+            int right_val = arr[right];
+            int right_count = 1;
+            while (right - 1 >= 0 && arr[right - 1] == right_val) {
                 right--;
                 right_count++;
             }
+            
             count += left_count * right_count;
-        } else if (sum < value) {
             left++;
-        } else {
+            right--;
+        } 
+        else if (sum < value) {
+            left++;
+        } 
+        else {
             right--;
         }
     }
     return count;
 }
 
-int binarySearchCount(int *arr, int left, int right, int target) {
-    int first_pos = -1;
+int countOccurrences(int *arr, int left, int right, int target) {
+    int first = -1, last = -1;
+
     int low = left, high = right;
     while (low <= high) {
         int mid = low + (high - low) / 2;
@@ -57,7 +69,7 @@ int binarySearchCount(int *arr, int left, int right, int target) {
             low = mid + 1;
         }
     }
-    first_pos = low;
+    first = low;
 
     low = left, high = right;
     while (low <= high) {
@@ -68,20 +80,16 @@ int binarySearchCount(int *arr, int left, int right, int target) {
             high = mid - 1;
         }
     }
-    int last_pos = high;
-
-    if (first_pos <= last_pos) {
-        return last_pos - first_pos + 1;
-    }
-    return 0;
+    last = high;
+    
+    return (first <= last) ? (last - first + 1) : 0;
 }
 
 int countPairs3(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len; ++i) {
         int target = value - arr[i];
-        int matches = binarySearchCount(arr, i + 1, len - 1, target);
-        count += matches;
+        count += countOccurrences(arr, i + 1, len - 1, target);
     }
     return count;
 }
